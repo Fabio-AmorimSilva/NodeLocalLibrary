c3d1f92244a038c06ff30455f7606c2295cf2643
Fabio <fabioamorim2025@hotmail.com>
2021-03-29 16:47:44 -0300

    Routes adicionadas

--- app.js
+++ app.js
@@ -10,8 +10,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//Routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
+var wiki = require('./routes/wiki.js');
+
 
 var app = express();
 
@@ -27,6 +31,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/wiki', wiki);
+app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
